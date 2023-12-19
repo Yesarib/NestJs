1634de773bb56755dc92abc0b270bf23dd9efa88
Barış Yesari <105274476+Yesarib@users.noreply.github.com>
2023-12-19 15:04:55 +0300

    docker and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmartkModule } from './bookmartk/bookmartk.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmartkModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmartkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
